cbf279b6efb322ae7bc181bc086c4b27d13c2e7e
Tomas Kislan <kislan.tomas@gmail.com>
2018-04-08 20:43:03 +0200

    Added unit tests

---
+++ src/__test__/utils.test.js
@@ -0,0 +1,20 @@
+import { parseAuthorizationHeader } from '../utils';
+
+function createReqWithAuthorization(username, password) {
+  const usernamePassword = `${username}:${password}`;
+  const authorizationHeader = `Basic ${Buffer.from(usernamePassword).toString('base64')}`;
+  return { headers: { authorization: authorizationHeader } };
+}
+
+test('Test authorization header parsing', () => {
+  const usernamePasswordPairs = [
+    ['test', 'test'],
+    ['somename@somedomain.net', 'NoOneWillGuessThisWithspecial***$@']
+  ];
+
+  for (const [inUsername, inPassword] of usernamePasswordPairs) {
+    const { username, secret } = parseAuthorizationHeader(createReqWithAuthorization(inUsername, inPassword));
+    expect(username).toEqual(inUsername);
+    expect(secret).toEqual(inPassword);
+  }
+});
