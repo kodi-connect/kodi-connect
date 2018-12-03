9cd0d2fbb7b691d0e573e69a2cef59845bfb7e13
Tomas Kislan <kislan.tomas@gmail.com>
2018-12-04 00:55:32 +0100

    Removed commented code

--- src/params.js
+++ src/params.js
@@ -11,22 +11,6 @@ mongoose.model('Params', ParamsSchema);
 
 export const ParamsModel = mongoose.model('Params');
 
-// async function getParam<T>(name: string, defaultValue: ?T): Promise<?T> {
-//   const param = await ParamsModel.findOne({ name }).lean();
-//   return (param && param.value) || defaultValue || undefined;
-// }
-//
-// async function setParam<T>(name: string, value: T): Promise<void> {
-//   await ParamsModel.findOneAndUpdate({ name }, { name, value }, { upsert: true });
-// }
-
-// function createParam<T>(name: string) {
-//   return {
-//     getValue: (defaultValue: ?T) => getParam(name, defaultValue),
-//     setValue: (value: T) => setParam(name, value),
-//   };
-// }
-
 class Param<T> {
   name: string;
   constructor(name: string) {
@@ -45,6 +29,3 @@ class Param<T> {
 
 export const AmazonCredentials: Param<?Object> = new Param('AmazonCredentials');
 export const AlexaSkills: Param<Array<string>> = new Param('AlexaSkills');
-
-// export const AmazonCredentials = createParam<Object>('AmazonCredentials');
-// export const AlexaSkills = createParam('AlexaSkills');
